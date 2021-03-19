14af6c349bc1f42322bbad68b8662abae71dd1bc
Chookity <jure.mohar5@gmail.com>
2021-03-19 14:46:08 +0100

    project cleanup, implement qtDesigner, on start show dot that follows users eyes

--- EyeTracker.py
+++ EyeTracker.py
@@ -1,10 +1,8 @@
+import numpy as np
 import tobii_research as tr
-import time
-
 
 class EyeTracker:
-    last_gaze_left_eye = 0,
-    last_gaze_right_eye = 0
+    gazeData = None,
 
     def __init__(self):
         self.found_eyetrackers = tr.find_all_eyetrackers()
@@ -16,20 +14,18 @@ class EyeTracker:
 
         print("init eyetracker class - found" + str(len(self.found_eyetrackers)))
 
-    def gaze_data_callback(gaze_data):
-        # Print gaze points of left and right eye
-        EyeTracker.last_gaze_left_eye = gaze_data['left_gaze_point_on_display_area']
-        EyeTracker.last_gaze_right_eye = gaze_data['right_gaze_point_on_display_area']
-        #
+    def gaze_data_callback(self, gaze_data):
+        self.gazeData = gaze_data
+
         # print("Left eye: ({gaze_left_eye}) \t Right eye: ({gaze_right_eye})".format(
         #     gaze_left_eye=gaze_data['left_gaze_point_on_display_area'],
         #     gaze_right_eye=gaze_data['right_gaze_point_on_display_area']))
-
 
     def start_tracking(self):
         if not self.tracker:
             print("error starting tracking - no device found")
             return
+
         self.tracker.subscribe_to(tr.EYETRACKER_GAZE_DATA, self.gaze_data_callback, as_dictionary=True)
 
         print("starting tracking")
@@ -42,4 +38,25 @@ class EyeTracker:
         self.tracker.unsubscribe_from(tr.EYETRACKER_GAZE_DATA, self.gaze_data_callback)
 
     def get_data(self):
-        print("s")
+        return self.gazeData
+
+    def getAvgEyePos(self):
+        # check to see if the eyetracker is connected and turned on
+        if self.tracker is None:
+            raise ValueError("There is no eyetracker.")
+
+        # access gaze data dictionary to get eye position tuples, given in
+        lOriginXYZ = self.gazeData['left_gaze_point_on_display_area']
+        rOriginXYZ = self.gazeData['right_gaze_point_on_display_area']
+
+        # create arrays with positions of both eyes on x, y, and z axes
+        xs = (lOriginXYZ[0], rOriginXYZ[0])
+        ys = (lOriginXYZ[1], rOriginXYZ[1])
+
+        # if all of the axes have data from at least one eye
+        if not (np.isnan(xs)).all() or not (np.isnan(ys)).all():
+            avgEyePos = (np.nanmean(xs), np.nanmean(ys))
+        else:
+            avgEyePos = (0, 0, 0)
+
+        return avgEyePos
--- main.py
+++ main.py
@@ -1,23 +1,137 @@
+import sys
+
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5 import QtCore
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import *
 import tobii_research as tr
-import time
 
-found_eyetrackers = tr.find_all_eyetrackers()
+class EyeTracker(QtCore.QObject):
+    positionChanged = QtCore.pyqtSignal(float, float)
+    test = pyqtSignal(object)
+    x = 0
+    y = 0
 
-my_eyetracker = found_eyetrackers[0]
-print("Address: " + my_eyetracker.address)
-print("Model: " + my_eyetracker.model)
-print("Name (It's OK if this is empty): " + my_eyetracker.device_name)
-print("Serial number: " + my_eyetracker.serial_number)
+    def __init__(self, tracker, parent=None):
+        super(EyeTracker, self).__init__(parent)
+        self._tracker = tracker
 
-def gaze_data_callback(gaze_data):
-    # Print gaze points of left and right eye
-    print("Left eye: ({gaze_left_eye}) \t Right eye: ({gaze_right_eye})".format(
-        gaze_left_eye=gaze_data['left_gaze_point_on_display_area'],
-        gaze_right_eye=gaze_data['right_gaze_point_on_display_area']))
+        self.test.connect(self.izpis)
 
-my_eyetracker.subscribe_to(tr.EYETRACKER_GAZE_DATA, gaze_data_callback, as_dictionary=True)
+    @property
+    def tracker(self):
+        return self._tracker
 
-time.sleep(5)
+    def start(self):
+        self.tracker.subscribe_to(
+            tr.EYETRACKER_GAZE_DATA, self._callback, as_dictionary=True
+        )
 
-my_eyetracker.unsubscribe_from(tr.EYETRACKER_GAZE_DATA, gaze_data_callback)
+    def _callback(self, gaze_data_):
+        #print(gaze_data_)
+        self.x = gaze_data_['left_gaze_point_on_display_area']
+        self.y = gaze_data_['right_gaze_point_on_display_area']
+        self.positionChanged.emit(
+            gaze_data_['left_gaze_point_on_display_area'],
+            gaze_data_['right_gaze_point_on_display_area']
+        )
 
+
+class UIMenu(QWidget):
+    def __init__(self, parent=None):
+        super(UIMenu, self).__init__(parent)
+        self.startBtn = QPushButton('Start', self)
+        self.startBtn.move(640, 480)
+
+        self.calibrateBtn = QPushButton('Start calibrate', self)
+        self.calibrateBtn.move(640, 580)
+
+class UICalibrate(QWidget):
+    def __init__(self, parent=None):
+        super(UICalibrate, self).__init__(parent)
+
+        #
+        # self.dot = QtWidgets.QPushButton(self)
+        # self.dot.setGeometry(QtCore.QRect(100, 100, 28, 28))
+        #self.dot.move(300, 300)
+
+        # self.tracker.positionChanged.connect(self.dot.move)
+        #self.tracker.positionChanged.connect(self.testEmit)
+
+        # self.tracker.positionChanged.connect(self.move)
+        self.backBtn = QPushButton("Back", self)
+        self.backBtn.move(640, 480)
+
+        self.topLeft = UICalibrationCircle(self)
+        self.topLeft.move(30, 30)
+
+        self.topRight = UICalibrationCircle(self)
+        self.topRight.move(1220, 30)
+
+        self.bottomLeft = UICalibrationCircle(self)
+        self.bottomLeft.move(30, 800)
+
+        self.bottomRight = UICalibrationCircle(self)
+        self.bottomRight.move(1220, 800)
+
+        self.dot = QtWidgets.QPushButton(self)
+        self.dot.setGeometry(QtCore.QRect(100, 100, 28, 28))
+        self.dot.move(300, 300)
+
+        eyetrackers = tr.find_all_eyetrackers()
+        self.tracker = EyeTracker(eyetrackers[0])
+        print(self.tracker.positionChanged)
+        self.tracker.positionChanged.connect(self.move)
+        self.tracker.start()
+
+
+    def move(self):
+        print("move")
+        # print(self.tracker.x)
+        # print(self.tracker.y)
+        # left = (self.tracker.x[0] * 1280, self.tracker.x[1] * 860)
+        # right = (self.tracker.y[1] * 1280, self.tracker.y[1] * 860)
+
+        # test tracking
+        x = (self.tracker.x[0] + self.tracker.x[0])/2 * 1280
+        y = (self.tracker.y[1] + self.tracker.y[1])/2 * 860
+
+        self.dot.move(x, y)
+        self.update()
+
+
+class UICalibrationCircle(QWidget):
+    def __init__(self, parent=None):
+        super(UICalibrationCircle, self).__init__(parent)
+
+        self.circle = QLabel(self)
+        self.circle.resize(30, 30)
+        self.circle.setStyleSheet("border: 3px solid blue; border-radius: 40px;")
+
+class MainWindow(QMainWindow):
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__(parent)
+        self.setFixedSize(1280, 860)
+        self.startUIMenu()
+
+    def startUIMenu(self):
+        self.Menu = UIMenu(self)
+        self.setWindowTitle("UIMenu")
+        self.setCentralWidget(self.Menu)
+        self.Menu.calibrateBtn.clicked.connect(self.startUICalibration)
+        self.show()
+
+    def startUICalibration(self):
+        self.Calibrate = UICalibrate(self)
+        self.setWindowTitle("Calibrate")
+        self.setCentralWidget(self.Calibrate)
+        self.Calibrate.backBtn.clicked.connect(self.startUIMenu)
+
+        self.show()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    w = MainWindow()
+    sys.exit(app.exec_())
